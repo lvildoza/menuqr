49b4a1267fe6850d508e2edb00d5b409042053ed
Leo Vildoza <leandro@losinformaticos.com.ar>
2024-08-21 01:52:36 -0300

    New proyect

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING, List
+import fastapi as _fastapi
+import sqlalchemy.orm as _orm
+
+import services as _services
+import schemas as _schemas
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import Session
+    
+app = _fastapi.FastAPI()
+
+@app.post("/api/contacts/", response_model=_schemas.Contact)
+async def create_contact(
+        contact: _schemas.CreateContact,
+        db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    return await _services.create_contact(contact=contact, db=db)
